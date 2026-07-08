a98f5f6e2d7b28e76a08fe699e00f297606c319e
2yt <2yt.dev.con@gmail.com>
2026-07-08 14:07:34 +0430

    Addition of a decorator file for decorators created within the application

---
+++ accounts/decorators.py
@@ -0,0 +1,29 @@
+from collections.abc import Callable
+from decorator import decorator
+from django.shortcuts import redirect
+
+def redirect_if_authenticated(path: str = 'dashboard'):
+    """
+    A decorator that checks if the user is logged in, 
+    redirects to the page the user specified in the route. 
+    If the route is empty, it defaults to the `dashboard` page
+    """
+    @decorator
+    def wrapper(func: Callable, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(path)
+        return func(request, *args, **kwargs)
+    return wrapper
+
+def redirect_if_not_authenticated(path: str = 'dashboard'):
+    """
+    A decorator that checks if the user is not logged in,
+    redirects to the page specified in the route.
+    If the route has no value, it defaults to the dashboard page
+    """
+    @decorator
+    def wrapper(func: Callable, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect(path)
+        return func(request, *args, **kwargs)
+    return wrapper
