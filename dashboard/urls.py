e04240720b36c497cd87405015673c7aa764549c
2yt <2yt.dev.con@gmail.com>
2026-07-08 14:21:37 +0430

    Changes implemented to modularize the project's view files

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_index_page, name='dashboard'),
+    path('', views.dashboard_page, name='dashboard'),
 ]
